ceaa4239fb5410f4f3d180c202e19ccee69c6389
Jack Leow <jackgene@gmail.com>
2025-03-30 19:17:54 -0700

    Log container creation error.

--- swift-async/Tests/EasyRacerTests/EasyRacerTests.swift
+++ swift-async/Tests/EasyRacerTests/EasyRacerTests.swift
@@ -7,6 +7,12 @@ import XCTest
 @testable import EasyRacer
 
 final class EasyRacerTests: XCTestCase {
+    lazy var docker = {
+        var logger = Logger(label: "docker-client")
+        logger.logLevel = .error
+        return DockerClient(logger: logger)
+    }
+    
     func testAllScenarios() async {
         // Set up
         var logger = Logger(label: "docker-client")
@@ -23,41 +29,42 @@ final class EasyRacerTests: XCTestCase {
                 ]
             )
         )
-        guard
-            let container = try? await docker.containers.create(spec: containerSpec)
-        else {
-            XCTFail("Failed to create container")
-            return
-        }
-        try? await docker.containers.start(container.id)
-        guard
-            let runningContainer = try? await docker.containers.get(container.id)
-        else {
-            XCTFail("Failed to start container")
-            return
-        }
-        let randomPort = runningContainer.networkSettings.ports["8080/tcp"]!!.first!.hostPort
-        let baseURL = URL(string: "http://localhost:\(randomPort)")!
-        // Wait for scenario server to start handling HTTP requests
-        while true {
-            do {
-                _ = try await URLSession.shared.data(from: baseURL)
-                break
-            } catch {
-                try? await Task.sleep(nanoseconds: 10_000_000) // 10ms
-                continue
+        do {
+            _ = try await docker.images.pull(byName: "ghcr.io/jamesward/easyracer:latest")
+            let container = try await docker.containers.create(spec: containerSpec)
+            try? await docker.containers.start(container.id)
+            guard
+                let runningContainer = try? await docker.containers.get(container.id)
+            else {
+                XCTFail("Failed to start container")
+                return
             }
+            let randomPort = runningContainer.networkSettings.ports["8080/tcp"]!!.first!.hostPort
+            let baseURL = URL(string: "http://localhost:\(randomPort)")!
+            // Wait for scenario server to start handling HTTP requests
+            while true {
+                do {
+                    _ = try await URLSession.shared.data(from: baseURL)
+                    break
+                } catch {
+                    try? await Task.sleep(nanoseconds: 10_000_000) // 10ms
+                    continue
+                }
+            }
+            
+            // Test
+            let results = await EasyRacer(baseURL: baseURL).scenarios()
+            XCTAssertEqual(results.count, 11, "Number of Scenarios")
+            for (idx, result) in results.enumerated() {
+                XCTAssertEqual(result, "right", "Scenario \(idx + 1)")
+            }
+            
+            // Tear down
+            try? await docker.containers.stop(container.id)
+        } catch {
+            XCTFail("Failed to create container: \(error)")
         }
         
-        // Test
-        let results = await EasyRacer(baseURL: baseURL).scenarios()
-        XCTAssertEqual(results.count, 11, "Number of Scenarios")
-        for (idx, result) in results.enumerated() {
-            XCTAssertEqual(result, "right", "Scenario \(idx + 1)")
-        }
-        
-        // Tear down
-        try? await docker.containers.stop(container.id)
         _ = try? await docker.containers.prune()
         try? await docker.shutdown()
     }
